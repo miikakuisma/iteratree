c7d5837ec161c7c9f3f8db1f4a6ac816a4df2603
Miika Kuisma <miikakuisma@Miikas-MBP.wlan>
2020-04-10 12:31:34 +0300

    select nodes with arrow keys

--- src/Tree.jsx
+++ src/Tree.jsx
@@ -1,65 +1,73 @@
 import React from "react";
 import Node from "./Node";
 import { arrayMove } from "./helpers";
-import { bindKeys } from "./keyboard";
 import "./styles.css";
 const traverse = require("traverse");
 
 export default function Tree({ tree, onRefresh, onUpdateNodeChildren }) {
   const [clipboard, setClipboard] = React.useState(null);
 
-  bindKeys([
-    {
-      key: "Backspace",
-      fn: () => {
+  window.onkeydown = e => {
+    switch (e.key) {
+      // DELETE
+      case "Backspace":
         deleteSelectedNodes();
-      }
-    },
-    {
-      key: "c",
-      withCmd: true,
-      fn: () => {
-        traverse(tree).forEach(function(x) {
-          if (typeof x === "object" && x.selected) {
-            copyNode(x);
-          }
-        });
-      }
-    },
-    {
-      key: "v",
-      withCmd: true,
-      fn: () => {
-        traverse(tree).forEach(function(x) {
-          if (typeof x === "object" && x.selected) {
-            pasteNode(x);
-          }
-        });
-      }
-    },
-    {
-      key: "ArrowLeft",
-      withCmd: true,
-      fn: () => {
-        traverse(tree).forEach(function(x) {
-          if (typeof x === "object" && x.selected) {
-            moveNode({ direction: 'left', node: x, parent: this.parent });
-          }
-        });
-      }
-    },
-    {
-      key: "ArrowRight",
-      withCmd: true,
-      fn: () => {
-        traverse(tree).forEach(function(x) {
-          if (typeof x === "object" && x.selected) {
-            moveNode({ direction: 'right', node: x, parent: this.parent });
-          }
-        });
-      }
+        break;
+      // COPY
+      case "c":
+        if (e.metaKey || e.ctrlKey) {
+          traverse(tree).forEach(function(x) {
+            if (typeof x === "object" && x.selected) {
+              copyNode(x);
+            }
+          });
+        }
+        break;
+      // PASTE
+      case "v":
+        if (e.metaKey || e.ctrlKey) {
+          traverse(tree).forEach(function(x) {
+            if (typeof x === "object" && x.selected) {
+              pasteNode(x);
+            }
+          });
+        }
+        break;
+      // MOVE
+      case "ArrowLeft":
+        if (e.metaKey || e.ctrlKey) {
+          traverse(tree).forEach(function(x) {
+            if (typeof x === "object" && x.selected) {
+              moveNode({ direction: 'left', node: x, parent: this.parent });
+            }
+          });
+        } else {
+          traverse(tree).forEach(function(x) {
+            if (typeof x === "object" && x.selected) {
+              selectChildNode({ direction: 'left', node: x, parent: this.parent });
+            }
+          });
+        }
+        break;
+      case "ArrowRight":
+        if (e.metaKey || e.ctrlKey) {
+          traverse(tree).forEach(function(x) {
+            if (typeof x === "object" && x.selected) {
+              moveNode({ direction: 'right', node: x, parent: this.parent });
+            }
+          });
+        } else {
+          traverse(tree).forEach(function(x) {
+            if (typeof x === "object" && x.selected) {
+              selectChildNode({ direction: 'right', node: x, parent: this.parent });
+            }
+          });
+        }
+        break;
+      default:
+        break;
     }
-  ]);
+  };
 
   function selectNode(node) {
     if (node.id === 0) {
@@ -99,7 +107,7 @@ export default function Tree({ tree, onRefresh, onUpdateNodeChildren }) {
     let pastedNode = clipboard;
     pastedNode.id = Date.now();
     pastedNode.selected = false;
-
+  
     if (node.options) {
       node.options.push(clipboard);
       onRefresh();
@@ -107,6 +115,28 @@ export default function Tree({ tree, onRefresh, onUpdateNodeChildren }) {
       node.options = [clipboard];
       onRefresh();
     }
+  }
+
+  function selectChildNode({ direction, node, parent }) {
+    // Selects child node left-right from current
+    let newParent = JSON.parse(JSON.stringify(parent.parent.node));
+    let oldIndex;
+    newParent.options.forEach((option, index) => {
+      if (option.id === node.id) {
+        oldIndex = index;
+      }
+    });
+    if (direction === 'right' && oldIndex < (newParent.options.length - 1)) {
+      const newIndex = oldIndex + 1;
+      newParent.options[oldIndex].selected = false;
+      newParent.options[newIndex].selected = true;
+    } 
+    if (direction === 'left' && oldIndex > 0) {
+      const newIndex = oldIndex - 1;
+      newParent.options[oldIndex].selected = false;
+      newParent.options[newIndex].selected = true;
+    }
+    onUpdateNodeChildren(parent.parent.node, newParent);
   }
 
   function moveNode({ direction, node, parent}) {
@@ -173,7 +203,7 @@ export default function Tree({ tree, onRefresh, onUpdateNodeChildren }) {
         onUpdateNode={(key, value) => {
           node[key] = value;
           onRefresh();
-          unselectAll();
+          // unselectAll();
         }}
         onAddNode={() => {
           if (node.options) {
