72a2a82da963b9b960d60485783c6c67017c8773
Miika Kuisma <miika@moreyes.fi>
2020-12-21 09:42:05 +0200

    removed env.js

--- src/env.js
+++
@@ -1,3 +0,0 @@
-export const SERVER_URL = 'https://parseapi.back4app.com/';
-export const APPLICATION_ID = 'o9nRKZJ6oS6b5G3lPgFmAEoSuARBZLEjghahwdS7';
-export const JAVASCRIPT_KEY = 'RDqcVOJSDAWGOiyjsxoWT3c7uVf6nCsXDp4VMQwp';
